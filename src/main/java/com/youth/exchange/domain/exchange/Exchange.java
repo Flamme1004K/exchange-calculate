9d84aa5cfdc86f19fc228149c6d82a776abee284
flamme <flamme1004@gmail.com>
2022-06-02 22:19:31 +0900

    build : lombok을 적용한다

--- src/main/java/com/youth/exchange/domain/exchange/Exchange.java
+++ src/main/java/com/youth/exchange/domain/exchange/Exchange.java
@@ -1,18 +1,17 @@
 package com.youth.exchange.domain.exchange;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table
+@NoArgsConstructor
 public class Exchange {
     @Id
     private Long id;
     private String pair;
     private Float rate;
-
-    protected Exchange() {
-
-    }
 }
