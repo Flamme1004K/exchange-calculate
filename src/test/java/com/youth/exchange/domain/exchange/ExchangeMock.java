f382a692dd54252765f0dc168acefd986ef08e7b
flamme <flamme1004@gmail.com>
2022-06-09 22:33:13 +0900

    test(ExchangeMock) : ExchangeMock 수정

--- src/test/java/com/youth/exchange/domain/exchange/ExchangeMock.java
+++ src/test/java/com/youth/exchange/domain/exchange/ExchangeMock.java
@@ -9,16 +9,16 @@ import java.util.stream.Collectors;
 public class ExchangeMock {
 
     private static final List<Exchange> exchanges = List.of(
-            new Exchange(1L, "USDAED", 3.673197f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(2L, "USDAFN", 76.088502f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(3L, "USDALL", 108.014949f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(4L, "USDAMD", 484.684999f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(5L, "USDANG", 1.78935f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(6L, "USDAOA", 308.428019f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(7L, "USDARS", 38.025498f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(8L, "USDAUD", 1.41645f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(9L, "USDAWG", 1.8005f, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
-            new Exchange(10L, "USDAZN", 1.704992f, LocalDateTime.of(2022, 1, 1, 1, 1, 1))
+            new Exchange(1L, "USDAED", 3.673197, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(2L, "USDAFN", 76.088502, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(3L, "USDALL", 108.014949, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(4L, "USDAMD", 484.684999, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(5L, "USDANG", 1.78935, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(6L, "USDAOA", 308.428019, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(7L, "USDARS", 38.025498, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(8L, "USDAUD", 1.41645, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(9L, "USDAWG", 1.8005, LocalDateTime.of(2022, 1, 1, 1, 1, 1)),
+            new Exchange(10L, "USDAZN", 1.704992, LocalDateTime.of(2022, 1, 1, 1, 1, 1))
     );
 
     public List<Exchange> getRecentRenewalExchanges(LocalDateTime localDateTime) {
